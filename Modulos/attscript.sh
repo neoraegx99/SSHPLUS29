56849ef8fac08fbd186f6f39379baab9624a6165
teamvpn <84849468+teamvpn@users.noreply.github.com>
2021-06-06 16:44:49 +0700

    Update attscript.sh

--- Modulos/attscript.sh
+++ Modulos/attscript.sh
@@ -22,7 +22,7 @@ touch $HOME/fim
 
  tput civis
 
-echo -ne "   \033[1;33mAGUARDE \033[1;37m- \033[1;33m["
+echo -ne "   \033[1;33mโปรดรอสักครู่... \033[1;37m- \033[1;33m["
 
 while true; do
 
@@ -44,11 +44,11 @@ while true; do
 
    tput dl1
 
-   echo -ne "   \033[1;33mAGUARDE \033[1;37m- \033[1;33m["
+   echo -ne "   \033[1;33mโปรดรอสักครู่... \033[1;37m- \033[1;33m["
 
 done
 
-echo -e "\033[1;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
+echo -e "\033[1;33m]\033[1;37m -\033[1;32m สำเร็จ !\033[1;37m"
 
 tput cnorm
 
@@ -62,7 +62,7 @@ fun_atts () {
 
   [[ -e /tmp/att ]] && rm /tmp/att
 
-	wget -c -P /home https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/versao
+	wget -c -P /home https://raw.githubusercontent.com/teamvpn/SSHPLUS29/master/Install/versao
 
 	[[ -f "/home/versao" ]] && mv /home/versao /tmp/att
 
@@ -70,21 +70,21 @@ fun_atts () {
 
 } > /dev/null 2>&1
 
-echo -e "                              \033[1;31mBy Crazy\033[1;36m"
+echo -e "                              \033[1;31mBy Lundy Kubtaphakon\033[1;36m"
 
-echo -e "   SSHPlus" | figlet
+echo -e "   KH-VPN" | figlet
 
 echo " "
 
-echo -e "   \033[1;32mVERIFICANDO ATUALIZACOES DISPONIVEIS\033[0m\n"
+echo -e "   \033[1;32mกำลังตรวจสอบการอัปเดตที่มีอยู่\033[0m\n"
 
 fun_bar 'fun_atts'
 
 [[ ! -f "/tmp/att" ]] && {
 
-	echo -e "\n\033[1;31m ERRO AO CONECTAR AO SERVIDOR\n"
+	echo -e "\n\033[1;31m เกิดข้อผิดพลาดในการเชื่อมต่อกับเซิร์ฟเวอร์\n"
 
-	echo -ne "\033[1;31m ENTER \033[1;33mpara retornar ao \033[1;32mMENU!\033[0m"; read
+	echo -ne "\033[1;31m ENTER \033[1;33mBACK \033[1;32mMENU!\033[0m"; read
 
 	menu
 
@@ -98,23 +98,23 @@ vrs2=$(sed -n '1 p' /tmp/att | sed -e 's/[^0-9]//ig')
 
 [[ "$vrs1" == "$vrs2" ]] && {
 
-  echo -e " \033[1;36m     O SCRIPT JA ESTA ATUALIZADO!\033[1;32m\n"
+  echo -e " \033[1;36m     สคริปต์ได้รับการปรับปรุงแล้ว!\033[1;32m\n"
 
   rm /tmp/att > /dev/null 2>&1
 
-  echo -e " \033[1;33m MAIS INFORMACOES \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@crazy_vpn\n"
+  echo -e " \033[1;33m ติดต่อ \033[1;31m(\033[1;36mFACEBOOK\033[1;31m): \033[1;37m@freenetvpndtac\n"
 
-  echo -ne " \033[1;31m ENTER \033[1;33mpara retornar ao \033[1;32mMENU!\033[0m"; read
+  echo -ne " \033[1;31m ENTER \033[1;33mBACK \033[1;32mMENU!\033[0m"; read
 
   menu
 
 } || {
 
-  echo -e "  \033[1;36mEXISTE UMA NOVA ATUALIZACAO DISPONIVEL!\033[1;33m\n"
+  echo -e "  \033[1;36mมีการอัปเดตใหม่พร้อมใช้งาน!\033[1;33m\n"
 
-  echo -e "  \033[1;33mMAIS INFORMACOES \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@crazy_vpn\n"
+  echo -e "  \033[1;33mติดต่อ \033[1;31m(\033[1;36mFACEBOOK\033[1;31m): \033[1;37m@freenetvpndtac\n"
 
-  echo -e "  \033[1;32mDETALHES DA ATUALIZACAO:\033[0m\n"
+  echo -e "  \033[1;32mอัพเดทรายละเอียด:\033[0m\n"
 
   while read linha; do
 
@@ -124,15 +124,15 @@ vrs2=$(sed -n '1 p' /tmp/att | sed -e 's/[^0-9]//ig')
 
   echo " "
 
-  echo -ne "  \033[1;32mDESEJA ATUALIZAR \033[1;31m? \033[1;33m[s/n]:\033[1;37m "; read res
+  echo -ne "  \033[1;32mต้องการที่จะปรับปรุง \033[1;31m? \033[1;33m[s/n]:\033[1;37m "; read res
 
   if [[ "$res" = s || "$res" = S ]];then
 
-    echo -e "\n\033[1;32m  INICIANDO ATUALIZACAO..."
+    echo -e "\n\033[1;32m  กำลังเริ่มอัปเดต..."
 
     sleep 3
 
-    wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Plus > /dev/null 2>&1
+    wget https://raw.githubusercontent.com/teamvpn/SSHPLUS29/master/Plus > /dev/null 2>&1
 
     chmod +x Plus
 
@@ -140,11 +140,11 @@ vrs2=$(sed -n '1 p' /tmp/att | sed -e 's/[^0-9]//ig')
 
     clear
 
-    echo -e "\033[1;32mSCRIPT ATUALIZADO COM SUCESSO\033[0m\n"
+    echo -e "\033[1;32mอัปเดตสคริปต์เรียบร้อยแล้ว\033[0m\n"
 
     rm /tmp/att > /dev/null 2>&1
 
-    echo -ne "\033[1;31mENTER \033[1;33mpara retornar ao \033[1;32mMENU!\033[0m"; read
+    echo -ne "\033[1;31mENTER \033[1;33mBACK \033[1;32mMENU!\033[0m"; read
 
     menu
 
